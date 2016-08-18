2c82f447fdddbc53ee9928de1a5c7e03763c5f31
Tristan Strange <tristan.strange@gmail.com>
2016-08-18 16:18:20 +0100

    Set up project.clj for deployment of snapshot

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject cc-services "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject org.clojars.triss/cc-services "0.1.0-SNAPSHOT"
+  :description "Idiomatic Clojure wrappers for Computational Creativity Web Services"
+  :url "https://github.com/triss/cc-services"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [ring/ring-codec "1.0.1"]])
+                 [ring/ring-codec "1.0.1"]]
+  :plugins [[lein-codox "0.9.6"]])
